08e3a8a03b7ece4497f7a629313cd55d34543cbe
ekayaint <ekayaint@gmail.com>
2023-05-15 21:13:45 +0300

    Initial Commit

---
+++ BasicAnimations/BasicAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicAnimationsApp.swift
+//  BasicAnimations
+//
+//  Created by ekayaint on 15.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct BasicAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
